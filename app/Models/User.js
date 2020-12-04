6219f2b7769b4bbaaba8aacb3c12006391349048
shalvah <diakon.ng@gmail.com>
2020-12-04 17:19:58 +0100

    Set up contact database schema + relations

--- app/Models/User.js
+++ app/Models/User.js
@@ -34,6 +34,10 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  contacts () {
+    return this.hasMany('App/Models/Contact')
+  }
 }
 
 module.exports = User
--- database/migrations/1607098241148_contact_schema.js
+++ database/migrations/1607098241148_contact_schema.js
@@ -7,6 +7,8 @@ class ContactSchema extends Schema {
   up () {
     this.create('contacts', (table) => {
       table.increments()
+      table.string('name').notNullable().unique()
+      table.integer('user_id').references('id').inTable('users')
       table.timestamps()
     })
   }
