6d31068dd7c3d482a9dc9ac0ba4ae3748ede9c64
shalvah <diakon.ng@gmail.com>
2021-01-30 20:15:24 +0100

    Use Elastic APM's trace ID

--- app/Middleware/SetTraceId.js
+++ app/Middleware/SetTraceId.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const TraceParent = require('traceparent');
 const apm = require('elastic-apm-node');
 
 class SetTraceId {
@@ -14,10 +13,7 @@ class SetTraceId {
 
     async handle({request}, next) {
         // Get ID from the traceparent header if available
-        const traceParentHeader = request.header('traceparent')
-        const traceId = traceParentHeader
-            ? TraceParent.fromString(traceParentHeader).traceId
-            : require('crypto').randomBytes(16).toString('hex')
+        const traceId = apm.currentTraceIds['trace.id']
         this.context.traceId = traceId
         await next()
     }
